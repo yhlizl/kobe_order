dfe2829f64cf81783b706d46520a4863d354fbe7
Royale Li <royale.li@sifive.com>
2024-07-21 21:56:42 +0800

    use postgres for prod

--- utils/db.tsx
+++ utils/db.tsx
@@ -1,28 +1,56 @@
 // db.tsx
 import mysql, { PoolOptions } from 'mysql2/promise';
+import { Pool as PgPool } from 'pg';
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '3306'),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-} as PoolOptions);
 
+let pool : any;
+
+if (process.env.NODE_ENV === 'production') {
+    pool = new PgPool({
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '5432'),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+} else {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '3306'),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    } as PoolOptions);
+}
 
 export async function initDb() {
+  if (process.env.NODE_ENV === 'production') {
+    await pool.query('CREATE DATABASE IF NOT EXISTS kobe_order');
+    await pool.query('CREATE SCHEMA IF NOT EXISTS kobe_order');
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS kobe_order.users (
+            userId SERIAL PRIMARY KEY,
+            email VARCHAR(255) UNIQUE,
+            name VARCHAR(255),
+            phone VARCHAR(255),
+            address VARCHAR(255),
+            password VARCHAR(255)
+        )
+    `);
+} else {
     await pool.query('CREATE DATABASE IF NOT EXISTS kobe_order');
     await pool.query('USE kobe_order');
     await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        userId INT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) UNIQUE,
-        name VARCHAR(255),
-        phone VARCHAR(255),
-        address VARCHAR(255),
-        password VARCHAR(255)
-      )
+        CREATE TABLE IF NOT EXISTS users (
+            userId INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(255) UNIQUE,
+            name VARCHAR(255),
+            phone VARCHAR(255),
+            address VARCHAR(255),
+            password VARCHAR(255)
+        )
     `);
+}
   }
   
 export default pool;
