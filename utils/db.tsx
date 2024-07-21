2c01aa60b9fb9f160641c413b007ab2731b682a3
Royale Li <royale.li@sifive.com>
2024-07-21 22:51:58 +0800

    finish migrate to postgres

--- utils/db.tsx
+++ utils/db.tsx
@@ -3,20 +3,30 @@ import mysql, { PoolOptions } from 'mysql2/promise';
 import { Pool as PgPool } from 'pg';
 
 
+let pool : any;
 
- const pool = new PgPool({
-      host: process.env.POSTGRES_HOST,
-      port: parseInt('5432'),
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      ssl: {
-        rejectUnauthorized: false
-    }
-  });
+if (process.env.NODE_ENV === 'production') {
+    pool = new PgPool({
+        host: process.env.POSTGRES_HOST,
+        port: parseInt('5432'),
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        ssl: {
+          rejectUnauthorized: false
+      }
+    });
+} else {
+    pool = new PgPool({
+        host: process.env.POSTGRES_HOST,
+        port: parseInt('5432'),
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+    });
+}
 
 export async function initDb() {
-  if (process.env.NODE_ENV === 'production') {
     let res = await pool.query(`
       SELECT EXISTS (
         SELECT FROM pg_database WHERE datname = 'verceldb'
@@ -38,20 +48,6 @@ export async function initDb() {
         password VARCHAR(255)
       )
     `);
-  } else {
-    await pool.query('CREATE DATABASE IF NOT EXISTS kobe_order');
-    await pool.query('USE kobe_order');
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        userId INT AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) UNIQUE,
-        name VARCHAR(255),
-        phone VARCHAR(255),
-        address VARCHAR(255),
-        password VARCHAR(255)
-      )
-    `);
-  }
 }
 
 export default pool;
