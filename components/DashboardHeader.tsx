2a275ed8a5fa86f4bffae53a833094bba1eaf16e
Royale Li <royale.li@sifive.com>
2024-07-22 23:24:18 +0800

    template plateform

---
+++ components/DashboardHeader.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from "react";
+import style from "./DashboardHeader.module.css";
+
+const Header: React.FC = () => {
+  
+
+  return (
+    <header className={style.dHeader}>
+    <h1>KOBE Pann 口碑烘焙坊 - 管理員儀表板</h1>
+  </header>
+  );
+};
+
+export default Header;
