b367c80d4bdce0daac2124053daf1e6358954ec3
Royale Li <royale.li@sifive.com>
2024-08-04 01:42:45 +0800

    change bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,19 +13,19 @@ const HomePage: React.FC = () => {
         <meta name="keywords" content="烘焙, KOBE Pann, 口碑烘焙坊, 精選食材, 匠心工藝, 精緻包裝" />
       </Head>
       <Layout>
-      <section className="relative rounded-lg p-8 mt-8 text-center shadow-md">
-        <div className="absolute inset-0 bg-slide bg-cover bg-no-repeat animate-slide"></div>
-        <div className="relative">
-          <h2>傳承手藝 匠心獨具</h2>
-          <p>KOBE Pann 口碑烘焙坊，用心烘焙每一口的幸福滋味</p>
-          <Link
-            href="/Products"
-            className="inline-block bg-[#8b4513] text-[#fff8e1] px-6 py-3 rounded-full no-underline font-bold hover:bg-[#a0522d] transition-all duration-300"
-          >
-            探索中秋月餅
-          </Link>
-        </div>
-      </section>
+      <section className="relative rounded-lg p-8 mt-8 text-center shadow-md lg:h-[500px]">
+  <div className="absolute inset-0 bg-slide bg-cover bg-center-top-200 bg-no-repeat animate-slide"></div>
+  <div className="relative">
+    <h2>傳承手藝 匠心獨具</h2>
+    <p>KOBE Pann 口碑烘焙坊，用心烘焙每一口的幸福滋味</p>
+    <Link
+      href="/Products"
+      className="inline-block bg-[#8b4513] text-[#fff8e1] px-6 py-3 rounded-full no-underline font-bold hover:bg-[#a0522d] transition-all duration-300"
+    >
+      探索中秋月餅
+    </Link>
+  </div>
+</section>
         <section className="flex justify-around mt-12 flex-wrap">
           <div className="bg-white rounded-lg p-6 w-full sm:w-1/2 md:w-1/3 text-center shadow-md transition-all duration-300 hover:-translate-y-2 my-4">
             <img
