d156d42c8ac2e0fd6c4a7fdc919401d7683f9d16
Royale Li <royale.li@sifive.com>
2024-08-04 23:21:39 +0800

    fix history

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,13 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div>
       <Navbar />
       {children}
+
+      <footer className="bg-[rgba(139,69,19,0.8)] text-[#fff8e1] text-center p-4 mt-12">
+          <p>&copy; 2023 KOBE Pann 口碑烘焙坊 版權所有</p>
+          <p className="mt-2">
+            地址：新竹市東區建中一路35號 | 電話：03-5719898
+          </p>
+      </footer>
     </div>
   );
 };
