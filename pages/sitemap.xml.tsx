15306993a23a4d129e42f15af78e816ef5fa871c
Royale Li <royale.li@sifive.com>
2024-08-15 21:45:27 +0800

    update sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -4,19 +4,43 @@ import { GetServerSideProps } from 'next';
 const Sitemap = () => {};
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const currentDate = new Date().toISOString();
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         <url>
             <loc>https://kobe-order.vercel.app/</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>daily</changefreq>
-            <priority>1.0</priority>
+            <lastmod>${currentDate}</lastmod>
+            <priority>1.00</priority>
         </url>
         <url>
             <loc>https://kobe-order.vercel.app/Products</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>daily</changefreq>
-            <priority>0.8</priority>
+            <lastmod>${currentDate}</lastmod>
+            <priority>0.80</priority>
+        </url>
+        <url>
+            <loc>https://kobe-order.vercel.app/About</loc>
+            <lastmod>${currentDate}</lastmod>
+            <priority>0.80</priority>
+        </url>
+        <url>
+            <loc>https://kobe-order.vercel.app/Contact</loc>
+            <lastmod>${currentDate}</lastmod>
+            <priority>0.80</priority>
+        </url>
+        <url>
+            <loc>https://kobe-order.vercel.app/Account</loc>
+            <lastmod>${currentDate}</lastmod>
+            <priority>0.80</priority>
+        </url>
+        <url>
+            <loc>https://kobe-order.vercel.app/Cart</loc>
+            <lastmod>${currentDate}</lastmod>
+            <priority>0.80</priority>
+        </url>
+        <url>
+            <loc>https://kobe-order.vercel.app/Login</loc>
+            <lastmod>${currentDate}</lastmod>
+            <priority>0.64</priority>
         </url>
     </urlset>
     `;
